1fcba89638ebce7fd6ccf192793b05fb3454d155
Aidan Follestad <drummer.aidan@gmail.com>
2013-02-24 19:07:06 -0600

    Severe ICS bug fix.

--- src/com/afollestad/overhear/utils/Store.java
+++ src/com/afollestad/overhear/utils/Store.java
@@ -6,8 +6,8 @@ import android.content.SharedPreferences;
 public class Store {
 
 	public static int i(Context context, String key, int def) {
-		SharedPreferences prefs = context.getSharedPreferences("store", def);
-		return prefs.getInt(key, 0);
+		SharedPreferences prefs = context.getSharedPreferences("store", 0);
+		return prefs.getInt(key, def);
 	}
 	
 	public static void put(Context context, String key, int value) {
