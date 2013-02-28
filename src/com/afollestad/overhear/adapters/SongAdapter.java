d7c3a9db767db9857f2732baf76aae9e9f3c1dce
Aidan Follestad <drummer.aidan@gmail.com>
2013-02-27 20:57:42 -0600

    Bug fixes.

--- src/com/afollestad/overhear/adapters/SongAdapter.java
+++ src/com/afollestad/overhear/adapters/SongAdapter.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 
 import com.afollestad.overhear.base.OverhearActivity;
 import com.afollestad.overhear.base.OverhearListActivity;
+import com.afollestad.overhear.queue.QueueItem;
 import com.afollestad.overhear.utils.MusicUtils;
 import com.afollestad.overhear.R;
 import com.afollestad.overhear.ui.AlbumViewer;
@@ -27,114 +28,114 @@ import com.afollestad.overhearapi.Song;
 
 public class SongAdapter extends CursorAdapter {
 
-    public SongAdapter(Context context, Cursor c, int flags) {
-        super(context, c, flags);
-        this.activity = (Activity)context;
-    }
+	public SongAdapter(Context context, Cursor c, int flags) {
+		super(context, c, flags);
+		this.activity = (Activity)context;
+	}
 
-    private Activity activity;
+	private Activity activity;
 
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.song_item, null);
-    }
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+		return LayoutInflater.from(context).inflate(R.layout.song_item, null);
+	}
 
-    public static View getViewForSong(final Activity context, final Song song, View view) {
-        if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.song_item, null);
-        }
+	public static View getViewForSong(final Activity context, final Song song, View view) {
+		if (view == null) {
+			view = LayoutInflater.from(context).inflate(R.layout.song_item, null);
+		}
 
-        ((TextView) view.findViewById(R.id.title)).setText(song.getTitle());
+		((TextView) view.findViewById(R.id.title)).setText(song.getTitle());
 
-        View options = view.findViewById(R.id.options);
-        options.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PopupMenu menu = new PopupMenu(context, view);
-                menu.inflate(R.menu.song_item_popup);
-                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem menuItem) {
-                        switch (menuItem.getItemId()) {
-                            case R.id.addToPlaylist: {
-                                AlertDialog diag = MusicUtils.createPlaylistChooseDialog(context, song, null, null);
-                                diag.show();
-                                return true;
-                            }
-                            case R.id.addToQueue: {
-                            	MusicUtils.addToQueue(context, song);
-                                return true;
-                            }
-                            case R.id.viewAlbum: {
-                                Album album = Album.getAlbum(context, song.getAlbum(), song.getArtist());
-                                context.startActivity(new Intent(context, AlbumViewer.class)
-                                        .putExtra("album", album.getJSON().toString()));
-                                return true;
-                            }
-                            case R.id.viewArtist: {
-                                Artist artist = Artist.getArtist(context, song.getArtist());
-                                context.startActivity(new Intent(context, ArtistViewer.class)
-                                        .putExtra("artist", artist.getJSON().toString()));
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-                });
-                menu.show();
-            }
-        });
+		View options = view.findViewById(R.id.options);
+		options.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				PopupMenu menu = new PopupMenu(context, view);
+				menu.inflate(R.menu.song_item_popup);
+				menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+					@Override
+					public boolean onMenuItemClick(MenuItem menuItem) {
+						switch (menuItem.getItemId()) {
+						case R.id.addToPlaylist: {
+							AlertDialog diag = MusicUtils.createPlaylistChooseDialog(context, song, null, null);
+							diag.show();
+							return true;
+						}
+						case R.id.addToQueue: {
+							MusicUtils.addToQueue(context, song);
+							return true;
+						}
+						case R.id.viewAlbum: {
+							Album album = Album.getAlbum(context, song.getAlbum(), song.getArtist());
+							context.startActivity(new Intent(context, AlbumViewer.class)
+							.putExtra("album", album.getJSON().toString()));
+							return true;
+						}
+						case R.id.viewArtist: {
+							Artist artist = Artist.getArtist(context, song.getArtist());
+							context.startActivity(new Intent(context, ArtistViewer.class)
+							.putExtra("artist", artist.getJSON().toString()));
+							return true;
+						}
+						}
+						return false;
+					}
+				});
+				menu.show();
+			}
+		});
 
-        ImageView peakOne = (ImageView) view.findViewById(R.id.peak_one);
-        ImageView peakTwo = (ImageView) view.findViewById(R.id.peak_two);
-        peakOne.setImageResource(R.anim.peak_meter_1);
-        peakTwo.setImageResource(R.anim.peak_meter_2);
-        AnimationDrawable mPeakOneAnimation = (AnimationDrawable) peakOne.getDrawable();
-        AnimationDrawable mPeakTwoAnimation = (AnimationDrawable) peakTwo.getDrawable();
+		ImageView peakOne = (ImageView) view.findViewById(R.id.peak_one);
+		ImageView peakTwo = (ImageView) view.findViewById(R.id.peak_two);
+		peakOne.setImageResource(R.anim.peak_meter_1);
+		peakTwo.setImageResource(R.anim.peak_meter_2);
+		AnimationDrawable mPeakOneAnimation = (AnimationDrawable) peakOne.getDrawable();
+		AnimationDrawable mPeakTwoAnimation = (AnimationDrawable) peakTwo.getDrawable();
 
-        Song focused = null;
-        boolean isPlaying = false; 
-        if(context instanceof OverhearActivity) {
+		QueueItem focused = null;
+		boolean isPlaying = false; 
+		if(context instanceof OverhearActivity) {
 			if(((OverhearActivity)context).getService() != null) {
-				focused = ((OverhearActivity)context).getService().getQueue().getFocused();
+				focused = ((OverhearActivity)context).getService().getQueue().getFocusedItem();
 				isPlaying = ((OverhearActivity)context).getService().isPlaying();
 			}
 		} else {
 			if(((OverhearListActivity)context).getService() != null) {
-				focused = ((OverhearListActivity)context).getService().getQueue().getFocused();
+				focused = ((OverhearListActivity)context).getService().getQueue().getFocusedItem();
 				isPlaying = ((OverhearListActivity)context).getService().isPlaying();
 			}
 		}
-        
-        if (focused != null && song.getId() == focused.getId()) {
-            peakOne.setVisibility(View.VISIBLE);
-            peakTwo.setVisibility(View.VISIBLE);
-            if (isPlaying) {
-                if (!mPeakOneAnimation.isRunning()) {
-                    mPeakOneAnimation.start();
-                    mPeakTwoAnimation.start();
-                }
-            } else {
-                mPeakOneAnimation.stop();
-                mPeakOneAnimation.selectDrawable(0);
-                mPeakTwoAnimation.stop();
-                mPeakTwoAnimation.selectDrawable(0);
-            }
-        } else {
-            peakOne.setVisibility(View.GONE);
-            peakTwo.setVisibility(View.GONE);
-            if (mPeakOneAnimation.isRunning()) {
-                mPeakOneAnimation.stop();
-                mPeakTwoAnimation.stop();
-            }
-        }
 
-        return view;
-    }
+		if (focused != null && song.getId() == focused.getSongId() && song.getPlaylistId() == focused.getPlaylistId()) {
+			peakOne.setVisibility(View.VISIBLE);
+			peakTwo.setVisibility(View.VISIBLE);
+			if (isPlaying) {
+				if (!mPeakOneAnimation.isRunning()) {
+					mPeakOneAnimation.start();
+					mPeakTwoAnimation.start();
+				}
+			} else {
+				mPeakOneAnimation.stop();
+				mPeakOneAnimation.selectDrawable(0);
+				mPeakTwoAnimation.stop();
+				mPeakTwoAnimation.selectDrawable(0);
+			}
+		} else {
+			peakOne.setVisibility(View.GONE);
+			peakTwo.setVisibility(View.GONE);
+			if (mPeakOneAnimation.isRunning()) {
+				mPeakOneAnimation.stop();
+				mPeakTwoAnimation.stop();
+			}
+		}
 
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        Song song = Song.fromCursor(cursor);
-        getViewForSong(activity, song, view);
-    }
+		return view;
+	}
+
+	@Override
+	public void bindView(View view, Context context, Cursor cursor) {
+		Song song = Song.fromCursor(cursor);
+		getViewForSong(activity, song, view);
+	}
 }
